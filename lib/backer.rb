43ab121512f8be4825eca65720d6b2753e34a05f
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-04 21:42:48 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,8 @@ class Backer
   end
 
   def back_project(project)
-    @backed_projects << project 
+    @backed_projects << project
     new_project = Project.new(project)
+    new_project.backers << self 
   end
 end
